65d277a401ffaf61519f39f47da69c1b62100d11
Muhammad Iqbal <81128836+mdiqball@users.noreply.github.com>
2021-06-24 11:49:00 +0700

    Add files via upload

---
+++ SQL_Data_Engineer_Challenge_With_SQL/Data/ms_produk.sql
@@ -0,0 +1,119 @@
+-- Create table ms_produk
+CREATE TABLE `dqlab`.`ms_produk` (
+    no_urut INT NOT NULL
+    , kode_produk VARCHAR(10) NOT NULL
+    , nama_produk VARCHAR(50) NOT NULL
+    , harga FLOAT NOT NULL
+    )
+
+-- Insert data into ms_produk
+
+INSERT INTO `dqlab`.`ms_produk` (
+    no_urut
+    , kode_produk
+    , nama_produk
+    , harga )
+VALUES (
+    1
+    ,'prod-01'
+    ,'Kotak Pensil DQLab'
+    ,'60500');
+
+INSERT INTO `dqlab`.`ms_produk` (
+    no_urut
+    , kode_produk
+    , nama_produk
+    , harga )
+VALUES (
+    2
+    ,'prod-02'
+    ,'Flashdisk DQLab 64 GB'
+    ,'55000');
+    
+INSERT INTO `dqlab`.`ms_produk` (
+    no_urut
+    , kode_produk
+    , nama_produk
+    , harga )
+VALUES (
+    3
+    ,'prod-03'
+    ,'Gift Voucher DQLab 100rb'
+    ,'100000');    
+    
+INSERT INTO `dqlab`.`ms_produk` (
+    no_urut
+    , kode_produk
+    , nama_produk
+    , harga )
+VALUES (
+    4
+    ,'prod-04'
+    ,'Flashdisk DQLab 32 GB'
+    ,'40000');
+    
+INSERT INTO `dqlab`.`ms_produk` (
+    no_urut
+    , kode_produk
+    , nama_produk
+    , harga )
+VALUES (
+    5
+    ,'prod-05'
+    ,'Gift Voucher DQLab 250rb'
+    ,'250000');
+    
+INSERT INTO `dqlab`.`ms_produk` (
+    no_urut
+    , kode_produk
+    , nama_produk
+    , harga )
+VALUES (
+    6
+    ,'prod-06'
+    ,'Pulpen Multifunction + Laser DQLab'
+    ,'92500');
+    
+INSERT INTO `dqlab`.`ms_produk` (
+    no_urut
+    , kode_produk
+    , nama_produk
+    , harga )
+VALUES (
+    7
+    ,'prod-07'
+    ,'Tas Travel Organizer DQLab'
+    ,'48000');
+    
+INSERT INTO `dqlab`.`ms_produk` (
+    no_urut
+    , kode_produk
+    , nama_produk
+    , harga )
+VALUES (
+    8
+    ,'prod-08'
+    ,'Gantungan Kunci DQLab'
+    ,'15800');
+    
+INSERT INTO `dqlab`.`ms_produk` (
+    no_urut
+    , kode_produk
+    , nama_produk
+    , harga )
+VALUES (
+    9
+    ,'prod-09'
+    ,'Buku Planner Agenda DQLab'
+    ,'92000');
+    
+INSERT INTO `dqlab`.`ms_produk` (
+    no_urut
+    , kode_produk
+    , nama_produk
+    , harga )
+VALUES (
+    10
+    ,'prod-10'
+    ,'Sticky Notes DQLab 500 sheets'
+    ,'55000');
---
+++ SQL_Data_Engineer_Challenge_With_SQL/Data/tr_penjualan_detail.sql
@@ -0,0 +1,166 @@
+-- Create table tr_penjualan_detail
+
+CREATE TABLE `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi VARCHAR(10) NOT NULL
+    , kode_produk VARCHAR(10) NOT NULL
+    , qty INT NOT NULL
+    , harga_satuan FLOAT NOT NULL
+    )
+
+
+-- Insert Data
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0001'
+    ,'prod-04'
+    ,3
+    ,40000);
+
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0001'
+    ,'prod-02'
+    ,1
+    ,55000);
+    
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0002'
+    ,'prod-08'
+    ,2
+    ,15800);
+
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0003'
+    ,'prod-10'
+    ,1
+    ,55000);
+
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0004'
+    ,'prod-09'
+    ,1
+    ,92000);
+
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0005'
+    ,'prod-06'
+    ,1
+    ,92500);
+
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0006'
+    ,'prod-08'
+    ,2
+    ,15800);
+
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0007'
+    ,'prod-08'
+    ,2
+    ,15800);
+
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0008'
+    ,'prod-07'
+    ,1
+    ,50000);
+
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0009'
+    ,'prod-01'
+    ,2
+    ,62500);
+    
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0010'
+    ,'prod-04'
+    ,3
+    ,48000);
+
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0010'
+    ,'prod-08'
+    ,1
+    ,15800);
+
+INSERT INTO `dqlab`.`tr_penjualan_detail` (
+    kode_transaksi
+    , kode_produk
+    , qty
+    , harga_satuan
+    )
+VALUES (
+    'tr-0010'
+    ,'prod-04'
+    ,1
+    ,40000);
